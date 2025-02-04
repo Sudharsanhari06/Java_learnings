642cfced4b7d0149141dd63e74b7d89ded7b2922
Sudharsanhari06 <sudharsanhari2006@gmail.com>
2025-02-04 16:27:18 +0530

    hello java

---
+++ hello.java
@@ -0,0 +1,10 @@
+class hello{
+    public static void main(String args[]){
+        System.out.println("hello");
+
+        int a=10;
+        int b=20;
+        System.out.print("sum "+(a+b));
+    }
+
+}
